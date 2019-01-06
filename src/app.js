8d74cfbc2e543270c609a5a669a8caac921e1473
Kevin Brail <kevinbrail@kevins-air.home>
2019-01-06 13:48:33 -0500

    Remove Dummy expense data

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ const store = configureStore();
 //store.dispatch(addExpense({description: 'Water Bill'}));
 //store.dispatch(addExpense({description: 'Gas Bill'}));
 
-const expenseOne = store.dispatch(addExpense({description:'Water Bill', note:'Water should be free', amount:500, createdAt:200} ));
-const expenseTwo = store.dispatch(addExpense({description:'Gas Bill', amount:475, note:'Gas is expensive', createdAt:1000} ));
-const expenseThree = store.dispatch(addExpense({description:'Rent', amount:10000, note:'Rent is too damn high!', createdAt:-200} ));
+// const expenseOne = store.dispatch(addExpense({description:'Water Bill', note:'Water should be free', amount:500, createdAt:200} ));
+// const expenseTwo = store.dispatch(addExpense({description:'Gas Bill', amount:475, note:'Gas is expensive', createdAt:1000} ));
+// const expenseThree = store.dispatch(addExpense({description:'Rent', amount:10000, note:'Rent is too damn high!', createdAt:-200} ));
 
 // store.dispatch(setTextFilter('Water'));
 // setTimeout(() => {
 //     store.dispatch(setTextFilter('Bill'));
 // }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
 console.log(visibleExpenses);
 
