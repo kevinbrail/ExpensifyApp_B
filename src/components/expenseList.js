35cc0bfa8cfddfca3a0d2b94a8841c0625755f8a
Kevin Brail <kevinabrail@gmail.com>
2019-01-07 10:05:54 -0500

    Fixed casing issue in expenseList.js

--- src/components/expenseList.js
+++ src/components/expenseList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ExpenseListItem from './ExpenseListItem';
+import ExpenseListItem from './expenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
